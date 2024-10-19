0ab544394be9393a03a8ae7d6bca98596d490f11
justcoom <vbruner11@mail.ru>
2024-10-19 15:07:24 +0700

    every word in string start with Upper letter

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -1,9 +1,37 @@
 /*
   Нужно написать функцию capitalizeWords, которая принимает строку и делает так, чтобы каждое слово начиналось с заглавной буквы.
-  Например, если передать строку "hello world from javascript", функция должна вернуть "Hello World From JavaScript".
+  Например, если передать строку "hellow world from javascript", функция должна вернуть "Hello World From JavaScript".
 
   🧙‍♂️Эту задачу можно решить при помощь цикла for. Рекомендуем реализовать этот вариант решения, как самый эффективный.
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  let result = str.slice(0, 1).toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === " ") {
+      result = result + " " + str.slice(i + 1, i + 2).toUpperCase()
+      i++
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
+
+// function variantTwo(str) {
+//   let result = str.split("").map((char) => {
+//     if (char === " ") {
+//       return char = char.toUpperCase()
+//     }
+//     return char
+//   }).join("")
+//   return result
+// }
+
+console.log(capitalizeWords('hello world from javascript'));
+console.log(capitalizeWords('I am try guy my friend'));
+
+// console.log(variantTwo('hello world from javascript'));
+
+
