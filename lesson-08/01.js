bbd33129002b9f2f8e843b094d6488518c421a8a
justcoom <vbruner11@mail.ru>
2024-10-30 15:11:10 +0700

    fix add pets to cart

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -68,3 +68,13 @@ clearCartButton.addEventListener('click', function () {
 })
 
 // Твой код:
+
+petShop.addEventListener('click', function(event) {
+  if (cart.length < 3) {
+    const petId = event.target.id
+    cart.push(petId)
+    updateCartDisplay()
+  } else {
+    messageBox.textContent = "Вы не можете добавить более 3 питомцев"
+  }
+})
